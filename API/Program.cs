4cd8c9af2b44659001388eea73985e2cd202ed08
dannybeaudoin28 <danny.beaudoin@student.sl.on.ca>
2024-01-20 22:38:31 -0500

    Connect front end to back end

--- API/Program.cs
+++ API/Program.cs
@@ -10,13 +10,17 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. Middleware must be in order
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+// Configure Http Requests
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
